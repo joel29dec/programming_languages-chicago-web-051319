1415348eb6f0f9e4c113070df68b27690c7f1dca
Joel S <joel29dec@gmail.com>
2019-04-10 19:23:48 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,39 +1,3 @@
-require 'pry'
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
- 
-  }
-}
-
-
-
-
 def reformat_languages(languages)
   combined = languages[:oo].merge(languages[:functional])
   languages[:functional].map do |k, v| 
