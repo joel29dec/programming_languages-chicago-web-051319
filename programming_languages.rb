e356279ee05e2d249ee4d1fc7a6d0ac4b3a94eef
Joel S <joel29dec@gmail.com>
2019-04-10 19:23:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -31,20 +31,7 @@ languages = {
   }
 }
 
-combined = languages[:oo].merge(languages[:functional])
 
-languages[:functional].map do |k, v| 
-  if combined.include?(k) 
-  combined[k].merge!(:style => [:functional]) 
-    end
-  end  
-languages[:oo].map do |k, v| 
-  if combined.include?(k) 
-  combined[k].merge!(:style => [:oo]) 
-    end
-  end
-combined[:javascript][:style] << :functional
-binding.pry
 
 
 def reformat_languages(languages)
