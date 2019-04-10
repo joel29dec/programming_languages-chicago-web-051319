3c52c2bc7856295076594a761ec173b1ebac925f
Joel S <joel29dec@gmail.com>
2019-04-10 19:04:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -34,9 +34,8 @@ languages = {
 combined = languages[:oo].merge(languages[:functional])
 #iterate over combined and check languages hash if its oo or functional if it is add symbol to :style =>
 languages[:oo].each do |k, v| 
-  binding.pry
   if combined.include?(k) 
-  combined[] 
+  puts combined[k] 
 end
 binding.pry
 k.each {|k, v| puts v}
